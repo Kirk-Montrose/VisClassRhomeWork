c140d33178806ebb0de9904845a439996afc21fc
Kirk Montrose <35209904+icysquirrel@users.noreply.github.com>
2021-03-04 17:20:45 -0500

    SimpleModelUpdates

--- Main Script.R
+++ Main Script.R
@@ -1,2 +1,8 @@
 setwd("/Users/kirkmontrose/Desktop/") #/Change this to point at you own location 
 dataFromDating <- read.csv("dating.csv")
+
+
+fit <- lm(wave~id , data = dataFromDating )
+
+
+print(summary(fit))
