2789ec2ee0d7b30e18019ae9519b43ed64090cda
Kirk Montrose <35209904+icysquirrel@users.noreply.github.com>
2021-03-04 17:27:15 -0500

    Made some more changes. Not sure if im going ni the correct direction yet, trying to filter for just a single wave

--- Main Script.R
+++ Main Script.R
@@ -1,8 +1,14 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+
 setwd("/Users/kirkmontrose/Desktop/") #/Change this to point at you own location 
 dataFromDating <- read.csv("dating.csv")
 
+filterData <- filter(dataFromDating,wave ==1) #Get Data from first wave.
 
 fit <- lm(wave~id , data = dataFromDating )
 
 
-print(summary(fit))
+print(summary(filterData))
