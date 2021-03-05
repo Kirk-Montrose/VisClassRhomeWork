0f01116b276d79ee316fe688bb635b80adc79af8
mcdonaldrichard11 <80132737+mcdonaldrichard11@users.noreply.github.com>
2021-03-05 17:53:39 -0500

    Update Main Script.R
    Added change to local path

--- Main Script.R
+++ Main Script.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(tidyr)
 
 
-setwd("~/Homework/VisClassRhomeWork/Data") #Set your on path
+setwd("~/Documents/Github/VisClassRhomeWork/Data") #Set your on path
 dataFromDating <- read.csv("dating.csv")
 filterData <- filter(dataFromDating,wave ==2) #Get Data from first wave.
 fit <- lm(wave~id , data = dataFromDating )
